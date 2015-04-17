e233d18f8a4e7fdcb02e9c16c0ac1ff536dc48cf
nemanja <nemanjakovach@gmail.com>
2015-04-17 14:04:21 +0000

    Corrected errors in models.py

--- notes/apps/writer/models.py
+++ notes/apps/writer/models.py
@@ -1,19 +1,29 @@
 from django.db import models
-#import datetime
+from django.conf import settings
+
 
 
 class Notebook(models.Model):
+
     name = models.CharField(max_length=30, unique=True)
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
     def __str__(self):
+        """Return the name of notebook"""
         return self.name
 
+
 class Note(models.Model):
+    """class
+    """
+
     title = models.CharField(max_length=30)
     contents = models.TextField(blank = True)
     date_of_create = models.DateField.auto_now_add('creation date')
     last_modified = models.DateTimeField.auto_now()
     notebook = models.ForeignKey(Notebook)
+
     def __str__(self):
+        """Return the title of note"""
         return self.title
 # Create your models here.

f8258b23e91b89f69c6edc400573f1bba11e56ac
Marija <marijamlsvljvc@gmail.com>
2015-04-20 14:46:04 +0000

    add class userManager and corected errors in models.py

--- notes/apps/auth/models.py
+++ notes/apps/auth/models.py
@@ -1,34 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
 class User(AbstractBaseUser):
-    password = models.CharField(_('password'), max_length=50)
     e_mail = models.EmailField(unique=True)
     is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'e_mail'
     REQUIRED_FIELDS = []
-
-    def get_username(self):
-        """ Returns the value of the field nominated by USERNAME_FIELD."""
-        return getattr(self, self.USERNAME_FIELD)
-
-    def is_authenticated(self):
-        """Return True. This is a way to tell if the user has been authenticated."""
-        return True
-
-    def set_password(self, raw_password):
-        self.password = make_password(raw_password)
-
-    def check_password(self, raw_password):
-        def setter(self, raw_password):
-            self.set_password(raw_password)
-            self.save(update_fields=["password"])
-        return check_password(raw_password, self.password, setter)
-
-    def set_unusable_password(self):
-        self.password = make_password(None)
 
 
     class Meta:
@@ -38,10 +18,32 @@ class User(AbstractBaseUser):
     def __str__(self):
         return self.username
 
+class UserManager(BaseUserManager):
+    def create_user(self, e_mail, password=None):
+         """
+        Creates and saves a User with the given email and password.
+        """
+        if not e_mail:
+            reise ValueError('Users must have an email address')
+
+        user = self.model(e_mail=self.normalize_email(e_mail),)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, e_mail, password=None):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(e_mail, password=password)
+        user.is_admin = True
+        user.save()
+        return user
+
 
 class UserProfile(models.Model):
     # This line is required. Links UserProfile to a User model instance.
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
     date_of_birth = models.DateField(blank=True)

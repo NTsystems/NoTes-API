b4cd37c43c70d5e101ac2d5dfb602cc73daa408d
Marija <marijamlsvljvc@gmail.com>
2015-04-22 13:47:47 +0000

    adapt admin.py

--- notes/apps/account/admin.py
+++ notes/apps/account/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import User, UserProfile
+from notes.apps.account.models import User, UserProfile
 
 admin.site.register(User)
 admin.site.register(UserProfile)

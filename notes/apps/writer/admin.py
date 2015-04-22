c40643ae0fa0e292a6d1cd043bd7fa4b7d11b4ee
nemanja <nemanjakovach@gmail.com>
2015-04-22 13:20:45 +0000

    Change settings.py,admin.py and add 0001_initial.py

--- notes/apps/writer/admin.py
+++ notes/apps/writer/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from notes.apps.writer import Notebook, Note
+
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'last_modified', 'date_of_create')
+    search_fields = ('title',)
+
+
+admin.site.register(Notebook)
+admin.site.register(Note, NoteAdmin)
+
+
+
 
 # Register your models here.

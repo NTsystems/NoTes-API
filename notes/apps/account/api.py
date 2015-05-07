d8baa0a16679f75515510a5445e5916904b47081
Marija <marijamlsvljvc@gmail.com>
2015-05-07 14:17:11 +0000

    Upgrading api.py

--- notes/apps/account/api.py
+++ notes/apps/account/api.py
@@ -1,48 +1,75 @@
-from django.shortcuts import render
-from rest_framework import status
+from rest_framework import status, authentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from notes.apps.account.models import User, UserProfile
 from notes.apps.account.resources import UserSerializer, ProfileSerializer
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from django.http import Http404
 
-
-class CreateUser(APIView):
+class Register(APIView):
     """Create new User."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
 
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
 
-            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+            return Response(status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class TokenView(APIView):
+
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        e_mail = request.data.get('e_mail')
+        password = request.data.get('password')
+        user = authenticate(username=e_mail, password=password)
+
+        if user is not None and user.is_active:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response(token.key, status=status.HTTP_201_CREATED)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
 class UpdateProfile(APIView):
     """Update user profile"""
-    permission_classes = (permissions.IsAuthenticated,)
+    queryset = UserProfile.objects.all()
+    serializer_class = ProfileSerializer
 
-    def put(self, request, id):
-        try:
-            profile = UserProfile.objects.get(id)
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (authentication.TokenAuthentication,)
 
-        except User.DoesNotExist:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-
-        if permissions.IsAuthenticated:
-            serializer = ProfileSerializer(profile, data=request.data['id'])
+    def put(self, request, id, format=None):
+        if request.user.is_authenticated():
+            profile = None
+            if hasattr(request.user, 'userprofile'):
+                profile = request.user.userprofile
+            serializer = self.serializer_class(profile, data=request.data, context={'request': request})
 
             if serializer.is_valid():
-                serializer.save()
-
-                return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+                serializer.save(user=request.user)
+                return Response(serializer.validated_data, status=status.HTTP_204_NO_CONTENT)
 
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
+
+
+
+
 
 
 
--- notes/apps/account/urls.py
+++ notes/apps/account/urls.py
@@ -1,7 +1,11 @@
 from notes.apps.account import api
 from django.conf.urls import url
+from rest_framework.authtoken import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^users/$', api.CreateUser.as_view()),
+    url(r'^users/$', api.Register.as_view()),
     url(r'^users/(?P<id>[0-9]+)/profile/$', api.UpdateProfile.as_view()),
-    ]
+    url(r'^tokens/$', api.TokenView.as_view()),
+]
+urlpatterns = format_suffix_patterns(urlpatterns)

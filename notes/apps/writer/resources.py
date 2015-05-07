a927b16251aae13feea6fd73b3aef41be0f788c1
nemanja <nemanjakovach@gmail.com>
2015-05-07 14:29:34 +0000

    Change resources.py and urls.py

--- notes/apps/writer/resources.py
+++ notes/apps/writer/resources.py
@@ -1,14 +1,25 @@
-#from django.forms import widgets
 from rest_framework import serializers
 from notes.apps.writer.models import Notebook, Note
 
 class NotebookSerializer(serializers.ModelSerializer):
+
+    # def create(self, validated_data):
+    #     notebook = Notebook.objects.create(user = self.context['request'].user, **validated_data)
+    #     return notebook
+
+
     class Meta:
         model = Notebook
         fields = ('id', 'name')
 
 
+
 class NoteSerializer(serializers.ModelSerializer):
+
+    # def create(self, validated_data):
+    #     note = Note.objects.create(notebook = self.context['notebook'], **validated_data)
+    #     return note
+
     class Meta:
         model = Note
         fields = ('id', 'title', 'contents')
--- notes/apps/writer/urls.py
+++ notes/apps/writer/urls.py
@@ -5,9 +5,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
 
     url(r'^$', api.NotebookList.as_view()),
-    url(r'^(?P<id>[0-9]+)/$', api.NotebookDetail.as_view()),
-    url(r'^(?P<pk>[0-9]+)/notes/$', api.NoteList.as_view()),
-    #url(r'^(?P<pk>[0-9]+)/notes/(?P<pk>[0-9]+)$', api.NoteList.NoteDetail.as_view()),
+    url(r'^(?P<notebook_id>[0-9]+)/$', api.NotebookDetail.as_view()),
+    url(r'^(?P<notebook_id>[0-9]+)/notes/$', api.NoteList.as_view()),
+    url(r'^(?P<notebook_id>[0-9]+)/notes/(?P<id>[0-9]+)/$', api.NoteDetail.as_view()),
 
 ]
 

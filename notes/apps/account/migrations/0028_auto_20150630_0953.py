800f22b92438cf8332e6ed48440d18b036c24c66
Marija <marijamlsvljvc@gmail.com>
2015-06-30 16:57:17 +0200

    Fix requirements.

---
+++ notes/apps/account/migrations/0028_auto_20150630_0953.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('account', '0027_auto_20150625_1338'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='user',
+            name='key_expires',
+            field=models.DateTimeField(default=datetime.datetime(2015, 6, 30, 9, 53, 41, 576577, tzinfo=utc)),
+        ),
+    ]
